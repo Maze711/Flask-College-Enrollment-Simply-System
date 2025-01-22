612b5153f5f81017b83568d0ba2636ce90ec6ca4
Maze711 <badlonmazeclarion@gmail.com>
2025-01-22 17:14:48 +0800

    it should be template with s

--- main.py
+++ main.py
@@ -1,40 +1,39 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///student_information.db'
 db = SQLAlchemy(app)
 
-# class Todo(db.Model):
-    # id = db.Column(db.Integer, primary_key=True)
-    # title = db.Column(db.String(200), nullable=False)
-    # description = db.Column(db.String(200), nullable=False)
-    # date_created = db.Column(db.DateTime, default=datetime.utcnow)
+class Student_Information(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    student_name = db.Column(db.String(200), nullable=False)
+    student_id = db.Column(db.String(200), nullable=False)
+    student_course = db.Column(db.String(200), nullable=False)
+    college_year = db.Column(db.String(200), nullable=False)
 
-    # def __repr__(self):
-    #     return '<Task %r>' % self.id
+    def __repr__(self):
+        return '<Student %r>' % self.id
 
 @app.route('/', methods=['POST', 'GET'])
-# def main():
-#     if request.method == 'POST':
-#         task_title = request.form['Title']
-#         task_description = request.form['Description']
-#         new_task = Todo(title=task_title, description=task_description)
-
-#         try:
-#             db.session.add(new_task)
-#             db.session.commit()
-#             return redirect('/')
-#         except:
-#             return 'There was an issue adding a task'
-    
-#     else:
-#         tasks = Todo.query.order_by(Todo.date_created).all()
-#         return render_template('index.html', tasks=tasks)
-
 def main():
-    return("Hello World")
+    if request.method == 'POST':
+        student_name = request.form['student_name']
+        student_id = request.form['student_id']
+        student_course = request.form['student_course']
+        college_year = request.form['college_year']
+        new_student = Student_Information(student_name=student_name, student_id=student_id, student_course=student_course, college_year=college_year)
+        try:
+            db.session.add(new_student)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding a student'
+    
+    else:
+        students = Student_Information.query.order_by(Student_Information.student_name).all()
+        return render_template('index.html', students=students)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
