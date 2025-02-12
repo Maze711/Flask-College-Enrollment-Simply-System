c88669c5d3ec6c74d5b8f0f72edc4799fccabed2
Maze711 <badlonmazeclarion@gmail.com>
2025-02-12 20:47:48 +0800

    Add admin_information model to manage admin data

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from sqlalchemy import Enum
 
 db = SQLAlchemy()
 
@@ -36,3 +37,21 @@ class student_information(db.Model):
 
     def __repr__(self):
         return '<StudentInformation %r>' % self.id
+
+class admin_information(db.Model):
+    __bind_key__ = 'admins'
+    id = db.Column(db.Integer, primary_key=True)
+    admin_number = db.Column(db.String(20), nullable=False)
+    admin_position = db.Column(Enum('Database Manager', 'Security', 'Developer', name='user_position'), nullable=False)
+    admin_lastname = db.Column(db.String(200), nullable=False)
+    admin_firstname = db.Column(db.String(200), nullable=False)
+    admin_middlename = db.Column(db.String(200), nullable=False)
+    admin_suffix = db.Column(db.String(10))
+    admin_email = db.Column(db.String(200), nullable=False)
+    admin_password = db.Column(db.String(200), nullable=False)
+    admin_status = db.Column(Enum('Active', 'Inactive', 'Missing', name='user_status'), nullable=False)
+    user_roll = db.Column(Enum('admin', 'student', 'faculty', name='user_roles'), nullable=False)
+    admin_created = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        return '<AdminInformation %r>' % self.id
